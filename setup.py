c9da9f19097ce91cbe9163724d4b248efa9c5cc9
Michael Barton <mail@michaelbarton.me.uk>
2016-04-30 14:14:32 -0700

    Create initial python module

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import biobox
+
+setup(
+    name                 = 'biobox',
+    version              = biobox.__version__,
+    description          = 'Create and run biobox Docker containers',
+    author               = 'bioboxes',
+    author_email         = 'mail@bioboxes.org',
+    url                  = 'http://bioboxes.org',
+    install_requires     = open('requirements.txt').read().splitlines(),
+
+    packages             = find_packages(),
+    include_package_data = True,
+
+    classifiers = [
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX'
+    ],
+)
