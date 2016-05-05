dfa350865c536dd80bdb2e6bc9b6db2ae85dca55
Michael Barton <mail@michaelbarton.me.uk>
2016-05-05 12:53:40 -0700

    Image availability now supports checking image digests

--- biobox/image/availability.py
+++ biobox/image/availability.py
@@ -3,18 +3,32 @@ import biobox.exception
 
 def get_image_tags(docker_dict):
     """
-    Returns list of all Docker image names with and without tags.
-    E.g. includes both python and python:2.7
+    Returns list of the names with and without tags for a given Docker image.
+    E.g. returns ["python", "python:2.7"]
     """
-    return reduce(lambda acc, x: acc + [x, x.split(":")[0]],
-            docker_dict['RepoTags'], [])
+    tags = docker_dict['RepoTags']
+    if (tags is None) or (tags == [u'<none>:<none>']):
+        return []
+    else:
+        return reduce(lambda acc, x: acc + [x, x.split(":")[0]], tags, [])
+
+def get_image_digests(docker_dict):
+    """
+    Returns list all digests, if they exist, for a Docker image
+    """
+    digests = docker_dict['RepoDigests']
+    if (digests is None) or (digests == [u'<none>@<none>']):
+        return []
+    else:
+        return digests
 
 def list_of_local_images():
     """
-    Returns a set containing all local docker image names and tags.
+    Returns a set containing all local docker image tags and digests.
     """
-    image_tags = map(get_image_tags, util.client().images())
-    return set(reduce(lambda acc, x: acc + x, image_tags, []))
+    images = util.client().images()
+    tags_and_digests = map(get_image_tags, images) + map(get_image_digests, images)
+    return set(reduce(lambda acc, x: acc + x, tags_and_digests, []))
 
 def is_image_available_locally(name):
     """
@@ -30,3 +44,4 @@ def get_image(name):
         output = util.client().pull(name)
         if "error" in output:
             raise biobox.exception.NoImageFound(name)
+    return True
