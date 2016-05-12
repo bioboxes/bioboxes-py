7c94af1d08864ec864ea9dbea8ec6a454e0aea1e
Michael Barton <mail@michaelbarton.me.uk>
2016-05-12 14:52:59 -0700

    Add documentation

--- biobox/container.py
+++ biobox/container.py
@@ -3,15 +3,27 @@ import biobox.util as util
 from requests.exceptions import ReadTimeout
 
 def metadata_lookup(path, container_id):
+    """
+    Look up metadata about the docker container for the given path
+    """
     return funcy.get_in(util.client().inspect_container(container_id), path)
 
 def did_exit_succcessfully(id_):
+    """
+    Checks if the container exited with 0 exit code
+    """
     return metadata_lookup(['State', 'ExitCode'], id_) == 0
 
 def is_running(id_):
+    """
+    Checks if the container is running
+    """
     return metadata_lookup(['State', 'Running'], id_)
 
 def collect_runtime_metrics(container_id, interval = 15, warmup = 1):
+    """
+    Collects runtime metrics about the container at the specified intervals
+    """
     import time
     time.sleep(warmup)
 
