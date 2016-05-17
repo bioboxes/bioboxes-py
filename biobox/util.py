232b89880f8fcca4e04c73b5e94f4cdd6cb138b2
Michael Barton <mail@michaelbarton.me.uk>
2016-05-17 15:18:52 -0700

    Downgrade docker API version to 1.20

--- biobox/util.py
+++ biobox/util.py
@@ -3,6 +3,6 @@ import docker.utils
 
 def client(timeout = 60):
     args = docker.utils.kwargs_from_env(assert_hostname = False)
-    args['version'] = '1.22'
+    args['version'] = '1.20'
     args['timeout'] = timeout
     return docker.Client(**args)
