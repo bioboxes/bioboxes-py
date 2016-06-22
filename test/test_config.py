6a4410c85867b3031031cd2acaf025c3caebd82d
Michael Barton <mail@michaelbarton.me.uk>
2016-06-22 15:56:26 -0700

    Add tests for different kinds of biobox entries

--- test/test_config.py
+++ test/test_config.py
@@ -8,24 +8,31 @@ reads_1 = os.path.join(dir_1, 'reads_1')
 reads_2 = os.path.join(dir_1, 'reads_2')
 reads_3 = os.path.join(dir_2, 'reads_3')
 
-def create_args(x):
-    return [{"fastq" : x}]
+def wrap(t, x):
+    return [{t : x}]
 
-def test_remap_args_with_single_file():
+def test_remap_args_with_single_entry():
     input    = [{"id" : 0 , "value" : reads_1, "type": "paired"}]
     expected = [{"id" : 0 , "value" : "/bbx/mount/0/reads_1", "type": "paired"}]
-    assert cnf.remap_biobox_input_paths(create_args(input)) == create_args(expected)
+    assert cnf.remap_biobox_input_paths(wrap("fastq", input)) == wrap("fastq", expected)
+    assert cnf.remap_biobox_input_paths(wrap("fasta", input)) == wrap("fasta", expected)
+    assert cnf.remap_biobox_input_paths(wrap("reference", input)) == wrap("reference", expected)
+
+def test_remap_args_with_single_fastq():
+    input    = [{"id" : 0 , "value" : reads_1, "type": "paired"}]
+    expected = [{"id" : 0 , "value" : "/bbx/mount/0/reads_1", "type": "paired"}]
+    assert cnf.remap_biobox_input_paths(wrap("fastq", input)) == wrap("fastq", expected)
 
 def test_remap_args_with_two_files_in_same_dir():
     input    = [{"id" : 0 , "value" : reads_1, "type": "paired"},
                 {"id" : 1 , "value" : reads_2, "type": "paired"}]
     expected = [{"id" : 0 , "value" : "/bbx/mount/0/reads_1", "type": "paired"},
                 {"id" : 1 , "value" : "/bbx/mount/0/reads_2", "type": "paired"}]
-    assert cnf.remap_biobox_input_paths(create_args(input)) == create_args(expected)
+    assert cnf.remap_biobox_input_paths(wrap("fastq", input)) == wrap("fastq", expected)
 
 def test_remap_args_with_two_files_in_different_dir():
     input    = [{"id" : 0 , "value" : reads_1, "type": "paired"},
                 {"id" : 1 , "value" : reads_3, "type": "paired"}]
-    output = cnf.remap_biobox_input_paths(create_args(input))
+    output = cnf.remap_biobox_input_paths(wrap("fastq", input))
     assert not os.path.dirname(output[0]['fastq'][0]['value']) == \
             os.path.dirname(output[0]['fastq'][1]['value'])
