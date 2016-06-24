fe7bae370411d4600171535915a3cefc657ec817
Michael Barton <mail@michaelbarton.me.uk>
2016-06-24 14:41:04 -0700

    Make volume testing deterministic

--- test/image/test_volumes.py
+++ test/image/test_volumes.py
@@ -36,8 +36,8 @@ def test_create_biobox_file_volume_string():
 #################################################
 
 import tempfile
-DIR_1 = tempfile.mkdtemp("1", "1")
-DIR_2 = tempfile.mkdtemp("2", "2")
+DIR_1 = tempfile.mkdtemp("1", "1__")
+DIR_2 = tempfile.mkdtemp("2", "2__")
 
 def create_tmp_file(dir_, name):
     path = os.path.join(dir_, name)
@@ -89,6 +89,9 @@ def test_create_remapping_with_file_and_dir_in_different_dir():
 #
 #################################################
 
+def simplify_vol_string(x):
+    return dict(zip(("host", "container", "access"), x.split(":")))
+
 def test_create_input_volume_strings_with_two_files():
     paths = ["/host/dir/0/file_0", "/host/dir/1/file_0"]
     volumes = vol.create_input_volume_strings(paths)
@@ -102,4 +105,5 @@ def test_create_input_volume_strings_with_single_directory():
 def test_create_input_volume_strings_with_file_and_dir_in_different_dir():
     paths = [create_tmp_file(DIR_1, "file_0"), create_tmp_dir(DIR_2, "dir_0")]
     volumes = vol.create_input_volume_strings(paths)
-    assert volumes == ["{}:/bbx/mount/0:ro".format(DIR_1), "{}:/bbx/mount/1:ro".format(DIR_2)]
+    vol_parts = map(simplify_vol_string, volumes)
+    assert vol_parts[0]["container"] != vol_parts[1]["container"]
