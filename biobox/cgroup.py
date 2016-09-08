adbe31098e1e26f6825b75ea35c8403052c3fcba
Michael Barton <mail@michaelbarton.me.uk>
2016-09-08 14:35:47 +0100

    Add function to collect cgroup metric entry from stream

--- biobox/cgroup.py
+++ biobox/cgroup.py
@@ -1,5 +1,24 @@
-import iso8601
+import iso8601, time
+import biobox.util      as util
+import biobox.container as ctn
 
 def time_diff_in_seconds(a, b):
+    """
+    Determines the difference in seconds between two string iso8601 dates.
+    """
     time_delta = iso8601.parse_date(b) - iso8601.parse_date(a)
     return time_delta.seconds
+
+
+def collect_metric(container_id):
+    """
+    Returns a cgroup metric dict for given container id, returns None if a
+    ReadTimeoutError is encoutered from the stream. Prevents race conditions
+    occuring from checking whether a container is running and then it subsequently
+    shutting down before collecting the cgroup metrics.
+    """
+    import requests.packages.urllib3.exceptions as ex
+    try:
+        return next(util.client().stats(container_id, decode = True))
+    except ex.ReadTimeoutError:
+        return None
--- test/test_cgroup.py
+++ test/test_cgroup.py
@@ -1,7 +1,23 @@
 import pytest
+import helper        as hlp
+import biobox.util   as util
 import biobox.cgroup as cgroup
 
 def test_time_diff_in_seconds():
     a = "2016-09-07T14:21:25.783051874Z"
     b = "2016-09-07T14:22:55.783051874Z"
     assert cgroup.time_diff_in_seconds(a, b) == 90
+
+
+@pytest.mark.slow
+def test_collect_metric_with_dead_container():
+    id_ = hlp.dummy_container(0.01)
+    assert cgroup.collect_metric(id_) == None
+    hlp.clean_up_container(id_)
+
+
+def test_collect_metric_with_running_container():
+    id_ = hlp.dummy_container(10, False)
+    assert 'read' in cgroup.collect_metric(id_)
+    util.client().kill(id_)
+    hlp.clean_up_container(id_)
