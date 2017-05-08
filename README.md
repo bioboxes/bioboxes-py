fb00ef3f47fbddfb85c21f45c48986784d55076b
Michael Barton <mail@michaelbarton.me.uk>
2017-05-08 11:20:59 -0400

    Project is moved to gitlab

---
+++ README.md
@@ -0,0 +1,5 @@
+## Moved
+
+This repository [has moved to gitlab][gl]
+
+[gl]: https://gitlab.com/bioboxes/bioboxes-py
--- biobox/util.py
+++
@@ -1,8 +0,0 @@
-import docker
-import docker.utils
-
-def client(timeout = 60):
-    args = docker.utils.kwargs_from_env(assert_hostname = False)
-    args['version'] = '1.20'
-    args['timeout'] = timeout
-    return docker.Client(**args)
